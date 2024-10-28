e2229480130f70df7519a9c161bfcf569394a794
IceBlizz6 <iceblizz6@proton.me>
2024-10-28 18:51:41 +0100

    Auto generate dist package json

---
+++ codegen-wrapper/scripts/make-dist-package.ts
@@ -0,0 +1,26 @@
+import { dirname, join } from "path"
+import { fileURLToPath } from "url"
+import { readFileSync, writeFileSync } from "fs"
+
+const scriptLocation = fileURLToPath(import.meta.url)
+const scriptsFolder = dirname(scriptLocation)
+const projectFolder = dirname(scriptsFolder)
+const originalPackageContent = readFileSync(join(projectFolder, "package.json"), "utf-8")
+const originalPackage = JSON.parse(originalPackageContent)
+
+const packageInfo = {
+    "name": originalPackage.name,
+    "version": originalPackage.version,
+    "type": "module",
+    "bin": {
+        "graphql-freeze": "./bin/generate.js"
+    },
+    "scripts": {
+		"postinstall": "./install.js"
+	}
+}
+
+writeFileSync(
+    join(projectFolder, "dist", "package.json"),
+    JSON.stringify(packageInfo, null, 2)
+)
